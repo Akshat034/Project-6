89b39d4119c742c71822d34ffe3f0754f643902b
Akshat034 <131787643+Akshat034@users.noreply.github.com>
2023-04-30 21:58:46 +0530

    Update firebase1.py

--- firebase1.py
+++ firebase1.py
@@ -19,19 +19,7 @@ while img_number <= count:
                 break
             print(file)
             img_number += 1
-            config = {
-                "apiKey": "AIzaSyCwjlcY-J2hr4fWaq4QqRe_a4EvVxP0wUY",
-                "authDomain": "pothole-fcc49.firebaseapp.com",
-                "projectId": "pothole-fcc49",
-                "storageBucket": "pothole-fcc49.appspot.com",
-                "messagingSenderId": "1098414391097",
-                "appId": "1:1098414391097:web:1ae28ebf65acbd84cdf2e8",
-                "measurementId": "G-GQK07PBWF4",
-                "serviceAccount": "serviceAccount.json",
-                "databaseURL": " https://pothole-fcc49-default-rtdb.firebaseio.com/"
-            }
-            firebase = pyrebase.initialize_app(config)
-            storage = firebase.storage()
+           
             path_on_cloud = file
             path_local = os.path.join(dir_path, file)
             storage.child(path_on_cloud).put(path_local)

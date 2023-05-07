5b82a8e22917fb8f7291f2abfbb451b8f5c19e89
Akshat034 <131787643+Akshat034@users.noreply.github.com>
2023-05-07 17:36:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
 import pyrebase
 
 
-config = {
-                "apiKey": "AIzaSyCwjlcY-J2hr4fWaq4QqRe_a4EvVxP0wUY",
-                "authDomain": "pothole-fcc49.firebaseapp.com",
-                "projectId": "pothole-fcc49",
-                "storageBucket": "pothole-fcc49.appspot.com",
-                "messagingSenderId": "1098414391097",
-                "appId": "1:1098414391097:web:1ae28ebf65acbd84cdf2e8",
-                "measurementId": "G-GQK07PBWF4",
-                "serviceAccount": "serviceAccount.json",
-                "databaseURL": " https://pothole-fcc49-default-rtdb.firebaseio.com/"
-}
+config =
 
 
 firebase = pyrebase.initialize_app(config)
